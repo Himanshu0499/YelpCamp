4d43ea09b71eca60256ead8c55a0ed7a794ab203
Himanshu0499 <88236305+Himanshu0499@users.noreply.github.com>
2021-08-02 19:47:18 +0530

    Added more images to seed campgrounds

--- seeds/seedHelpers.js
+++ seeds/seedHelpers.js
@@ -45,27 +45,36 @@ module.exports.places = [
 
 module.exports.images = [
     {
-      url: 'https://res.cloudinary.com/dnzjrylqa/image/upload/v1626869921/YelpCamp/dw6tjmmmbfgtvioensk7.jpg',
-      filename: 'YelpCamp/dw6tjmmmbfgtvioensk7'
+      url: 'https://res.cloudinary.com/dnzjrylqa/image/upload/v1627913065/YelpCamp/lwzplwppfy2lo0jrwcxv.jpg',
+      filename: 'YelpCamp/lwzplwppfy2lo0jrwcxv'
     },
     {
-      url: 'https://res.cloudinary.com/dnzjrylqa/image/upload/v1626869927/YelpCamp/wkrfajjsekh9y2rwn5o8.jpg',
-      filename: 'YelpCamp/wkrfajjsekh9y2rwn5o8'
+      url: 'https://res.cloudinary.com/dnzjrylqa/image/upload/v1627913112/YelpCamp/x8ozf6h4arc0qgpxmyud.jpg',
+      filename: 'YelpCamp/x8ozf6h4arc0qgpxmyud'
     },
     {
-      url: 'https://res.cloudinary.com/dnzjrylqa/image/upload/v1626869930/YelpCamp/yn2uldnfys1cbjf6vyyz.jpg',
-      filename: 'YelpCamp/yn2uldnfys1cbjf6vyyz'
+      url: 'https://res.cloudinary.com/dnzjrylqa/image/upload/v1627913112/YelpCamp/vjnxpfh1i3danwgknbap.jpg',
+      filename: 'YelpCamp/vjnxpfh1i3danwgknbap'
     },
     {
-      url: 'https://res.cloudinary.com/dnzjrylqa/image/upload/v1626869935/YelpCamp/fulkydbjisujlp21g46d.jpg',
-      filename: 'YelpCamp/fulkydbjisujlp21g46d'
+      url: 'https://res.cloudinary.com/dnzjrylqa/image/upload/v1627913154/YelpCamp/cezkxwx4c1iekg3ja7zp.jpg',
+      filename: 'YelpCamp/cezkxwx4c1iekg3ja7zp'
     },
     {
-      url: 'https://res.cloudinary.com/dnzjrylqa/image/upload/v1626869938/YelpCamp/w1vnxyqnbvufy2ehd4yk.jpg',
-      filename: 'YelpCamp/w1vnxyqnbvufy2ehd4yk'
+      url: 'https://res.cloudinary.com/dnzjrylqa/image/upload/v1627913155/YelpCamp/u9amxgrsch7jffnco3cm.jpg',
+      filename: 'YelpCamp/u9amxgrsch7jffnco3cm'
     },
     {
-      url: 'https://res.cloudinary.com/dnzjrylqa/image/upload/v1626869946/YelpCamp/hwq5oq4gpfzrosojt8wa.jpg',
-      filename: 'YelpCamp/hwq5oq4gpfzrosojt8wa'
+      url: 'https://res.cloudinary.com/dnzjrylqa/image/upload/v1627913176/YelpCamp/skw9khe5faq6sqtfnrdn.jpg',
+      filename: 'YelpCamp/skw9khe5faq6sqtfnrdn'
+    },
+    {
+      url: 'https://res.cloudinary.com/dnzjrylqa/image/upload/v1627913174/YelpCamp/xu4rfi2c2z00lwsmmfol.jpg',
+      filename: 'YelpCamp/xu4rfi2c2z00lwsmmfol'
+    },
+    {
+      url: 'https://res.cloudinary.com/dnzjrylqa/image/upload/v1627913256/YelpCamp/szd0662fwhlt7ptncwvf.jpg',
+      filename: 'YelpCamp/szd0662fwhlt7ptncwvf'
     }
-  ]
+  
+]
